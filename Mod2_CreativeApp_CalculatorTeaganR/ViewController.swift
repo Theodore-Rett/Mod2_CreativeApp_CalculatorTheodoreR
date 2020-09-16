f657cd406fcf867f2c9db08b3871db376055dbe3
TeaganRett <teagan.rett@gmail.com>
2020-09-16 14:30:11 -0500

    Initial Commit

---
+++ Mod2_CreativeApp_CalculatorTeaganR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mod2_CreativeApp_CalculatorTeaganR
+//
+//  Created by Tiger Coder on 9/16/20.
+//  Copyright © 2020 Tiger Coder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
